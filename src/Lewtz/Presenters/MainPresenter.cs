6abf8f366f8f4d52a89853fa91d06f8d31de9fc8
Tommy Long <pete_201@hotmail.com>
2015-07-23 23:00:34 +0100

    Introduced basics tests for MainPresenter

--- src/Lewtz/Presenters/MainPresenter.cs
+++ src/Lewtz/Presenters/MainPresenter.cs
@@ -1,9 +1,16 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace Lewtz.Presenters
 {
 	internal sealed class MainPresenter
 	{
+		public MainPresenter(object lootGenerator)
+		{
+			if (lootGenerator == null)
+				throw new ArgumentNullException(nameof(lootGenerator));
+		}
+
 		internal void Exit()
 		{
 			Application.Exit();
---
+++ tests/Lewtz.Tests/MainPresenterTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Lewtz.Presenters;
+using System;
+using TestAttributes;
+
+namespace Lewtz.Tests
+{
+	public class MainPresenterTests
+	{
+		public class Constructor
+		{
+			private static object DefaultGenerator
+			{
+				get
+				{
+					return new object();
+				}
+			}
+
+			[Unit]
+			public static void WhenLootGeneratorIsNullThenThrowsException()
+			{
+				Action act = () => new MainPresenter(null);
+				act.ShouldThrow<ArgumentNullException>();
+			}
+
+			[Unit]
+			public static void WhenLootGeneratorProvidedThenDoesNotThrowException()
+			{
+				Action act = () => new MainPresenter(DefaultGenerator);
+				act.ShouldNotThrow<ArgumentNullException>();
+			}
+		}
+	}
+}
